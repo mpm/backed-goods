689f189566acf6b871805b8750fa8764b518e369
Malte Münchert <malte.muenchert@gmx.net>
2015-05-26 02:49:11 +0200

    Add work from my first hacking session on 22/23rd May 2015

---
+++ js/game.js
@@ -0,0 +1,29 @@
+
+
+var keyPressed = false;
+
+function gameLoop() {
+  var player = currentLevel.player;
+  if (input.isDown('UP')) {
+    player.changeDirection(Direction.UP);
+    player.move();
+  } else if (input.isDown('DOWN')) {
+    player.changeDirection(Direction.DOWN);
+    player.move();
+  } else if (input.isDown('LEFT')) {
+    player.changeDirection(Direction.LEFT);
+    player.move();
+  } else if (input.isDown('RIGHT')) {
+    player.changeDirection(Direction.RIGHT);
+    player.move();
+  }
+  currentLevel.drawMovables();
+  requestAnimationFrame(gameLoop);
+}
+
+var currentLevel;
+window.onload = function() {
+  currentLevel = new Level(levels[2]);
+  currentLevel.drawMaze();
+  gameLoop();
+};
