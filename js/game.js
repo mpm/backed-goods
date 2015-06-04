bf9f86ea79ea9595fbd7e04ca2bfa2bc2e7b890e
Malte Münchert <malte.muenchert@gmx.net>
2015-06-05 01:21:43 +0200

    Add level transitions when exit is reached, refactor game class

--- js/game.js
+++ js/game.js
@@ -1,30 +1,46 @@
+var Game = function(config) {
+  var story = _.map(config.levels, function(index) { return Levels[index]; });
+  var currentLevel = null;
 
+  var gameLoop = function() {
+    var player = currentLevel.player;
+    if (input.isDown('UP')) {
+      player.changeDirection(Direction.UP);
+      player.move();
+    } else if (input.isDown('DOWN')) {
+      player.changeDirection(Direction.DOWN);
+      player.move();
+    } else if (input.isDown('LEFT')) {
+      player.changeDirection(Direction.LEFT);
+      player.move();
+    } else if (input.isDown('RIGHT')) {
+      player.changeDirection(Direction.RIGHT);
+      player.move();
+    }
+    currentLevel.drawMovables();
+    currentLevel.drawMaze();
+    if (currentLevel.isActive()) {
+      requestAnimationFrame(gameLoop);
+    }
+  };
 
-var keyPressed = false;
+  var levelCompleted = function(score) {
+    if (story.length > 0) {
+      startLevel();
+    }
+  };
 
-function gameLoop() {
-  var player = currentLevel.player;
-  if (input.isDown('UP')) {
-    player.changeDirection(Direction.UP);
-    player.move();
-  } else if (input.isDown('DOWN')) {
-    player.changeDirection(Direction.DOWN);
-    player.move();
-  } else if (input.isDown('LEFT')) {
-    player.changeDirection(Direction.LEFT);
-    player.move();
-  } else if (input.isDown('RIGHT')) {
-    player.changeDirection(Direction.RIGHT);
-    player.move();
-  }
-  currentLevel.drawMovables();
-  currentLevel.drawMaze();
-  requestAnimationFrame(gameLoop);
-}
+  var startLevel = function() {
+    currentLevel = new Level(story.shift());
+    currentLevel.onExit(levelCompleted);
+    currentLevel.drawMaze();
+    gameLoop();
+  };
 
-var currentLevel;
+  startLevel();
+};
+
 window.onload = function() {
-  currentLevel = new Level(levels[2]);
-  currentLevel.drawMaze();
-  gameLoop();
+  //var game = Game({levels: [4]});
+  var game = Game({levels: [0,1,2,3,4,5,6]});
 };
