996b04c42f14d5da8d7f18dbf712f8ae8e716111
Malte Münchert <malte.muenchert@gmx.net>
2015-06-07 11:58:39 +0200

    Change game loop to a different frame rate [experimental]

--- js/game.js
+++ js/game.js
@@ -1,27 +1,36 @@
 var Game = function(config) {
   var story = _.map(config.levels, function(index) { return Levels[index]; });
   var currentLevel = null;
+  var frameNeeded = true;
 
   var gameLoop = function() {
+    if (frameNeeded) {
+      frameNeeded = false;
+      setTimeout(function() { frameNeeded = true; }, 1000 / 50);
+      gameStep();
+    }
+    requestAnimationFrame(gameLoop);
+  };
+
+  var gameStep = function() {
     var player = currentLevel.player;
-    if (input.isDown('UP')) {
-      player.changeDirection(Direction.UP);
-      player.move();
-    } else if (input.isDown('DOWN')) {
-      player.changeDirection(Direction.DOWN);
-      player.move();
-    } else if (input.isDown('LEFT')) {
-      player.changeDirection(Direction.LEFT);
-      player.move();
-    } else if (input.isDown('RIGHT')) {
-      player.changeDirection(Direction.RIGHT);
-      player.move();
+    if (currentLevel.isActive()) {
+      if (input.isDown('UP')) {
+        player.changeDirection(Direction.UP);
+        player.move();
+      } else if (input.isDown('DOWN')) {
+        player.changeDirection(Direction.DOWN);
+        player.move();
+      } else if (input.isDown('LEFT')) {
+        player.changeDirection(Direction.LEFT);
+        player.move();
+      } else if (input.isDown('RIGHT')) {
+        player.changeDirection(Direction.RIGHT);
+        player.move();
+      }
     }
     currentLevel.drawMovables();
     currentLevel.drawMaze();
-    if (currentLevel.isActive()) {
-      requestAnimationFrame(gameLoop);
-    }
   };
 
   var levelCompleted = function(score) {
