b03ebaa1d524debc8967a710e8873f5aaf1eeaaf
Malte Münchert <malte.muenchert@gmx.net>
2015-06-11 00:01:03 +0200

    Properly clear layer in old sprite position instead of using a padding

--- js/screen.js
+++ js/screen.js
@@ -10,18 +10,22 @@ var Screen = function() {
     layer.clearRect(0, 0, _maze.width, _maze.height);
   };
 
-  var _drawBlock = function(layerName, x, y, index, clearBackground) {
+  var _drawBlock = function(layerName, x, y, index, options) {
       if (index == 0) { index = 99 };
       var sX = x * 10 * _magnify;
       var sY = y * 10 * _magnify;
       var sWidth = 10 * _magnify;
       var layer = layers[layerName];
-      if (clearBackground) {
+      if (options && options.oldX) {
         var padding = 5;
-        layer.clearRect(sX - padding,
-                        sY - padding,
-                        sWidth + padding * 2,
-                        sWidth + padding * 2);
+        layer.clearRect(options.oldX * 10 * _magnify,
+                        options.oldY * 10 * _magnify,
+                        sWidth,
+                        sWidth);
+        //layer.clearRect(sX - padding,
+                        //sY - padding,
+                        //sWidth + padding * 2,
+                        //sWidth + padding * 2);
       }
       layer.drawImage(sprites,
           0, (index - 1) * 10,
@@ -32,11 +36,11 @@ var Screen = function() {
     clearLayer: function(layerName) {
       clear(layers[layerName]);
     },
-    drawBlock: function(layerName, x, y, index) {
-      _drawBlock(layerName, x, y, index);
+    drawBlock: function(layerName, x, y, index, options) {
+      _drawBlock(layerName, x, y, index, options);
     },
-    renderMovable: function(x, y, type) {
-      _drawBlock('sprites', x, y, type, true);
+    renderMovable: function(x, y, type, options) {
+      _drawBlock('sprites', x, y, type, options);
     }
   };
 };
